f050002d68ec54dee0cd1820f092be0cf6c08f35
LonelyBush <lonely.bush68@gmail.com>
2024-04-14 16:52:17 +0300

    feat: add complete queuPromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -147,8 +147,13 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((promChain, currProm) => {
+    return promChain.then(async (val) => {
+      const value = await currProm;
+      return val + value;
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
